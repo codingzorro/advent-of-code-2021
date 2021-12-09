895f8176a46c32615429629bb6e2ed5fba2d99cb
Jorge Duenas <jorge@duenas.at>
2021-12-09 23:28:37 +0100

    create tables with cells

--- advent/src/advent/day04.clj
+++ advent/src/advent/day04.clj
@@ -1,30 +1,34 @@
 (ns advent.day04
-  (:require [clojure.string :as s]
-            [clojure.pprint :as pp]))
+  (:require [clojure.string :as s]))
 
 (def ^:private sample-input "resources/day04-test-input.txt")
 (def ^:private input-txt    "resources/day04-input.txt")
 
 (defrecord Game [numbers boards])
+(defrecord Cell [value x y])
 
 (defn load-numbers [line]
   (->> line
     (#(s/split % #",") ,,,)
     (map #(Integer. %) ,,,)
-    (into [] ,,,)
-    ))
+    (into [] ,,,)))
 
 (defn parse-line [line]
   (->> line
     (#(s/split % #" ") ,,,)
     (filter seq ,,,)
-    (map #(Integer. %) ,,,)
-    ))
+    (map #(Integer. %) ,,,)))
 
 (defn to-array-of-arrays [lines]
   (->> lines
-    (map parse-line ,,,)
-    ))
+    (map parse-line ,,,)))
+
+(defn make-board [board]
+  (let [coords (for [x (range 5) y (range 5)] [x y])]
+    (->> board
+      (reduce into [] ,,,)
+      ((fn [cells] (map #(vector %1 %2) cells coords)) ,,,)
+      (map (fn [[value [x y]]] (->Cell value x y)) ,,,))))
 
 (defn make-boards [lines]
   (->> lines
@@ -32,16 +36,15 @@
     (map #(drop 1 %) ,,,)
     (map #(into [] %) ,,,)      ; each array is one board, i.e. has n lines
     (map to-array-of-arrays ,,,)
-  ))
+    (map make-board ,,,)))
 
 (defn load-lines [file-name]
   (->> file-name
     (slurp)
     (#(s/split % #"\r\n") ,,,)))
 
-(def lines (load-lines sample-input))
-(def game (->Game (load-numbers(first lines))
-                   (make-boards (rest lines))))
-
-;(pp/pprint (seq (first (second (first (:boards game))))))
-(pp/pprint game)
+; ---
+(defn make-game []
+  (let [lines (load-lines sample-input)])
+    (->Game (load-numbers(first lines))
+            (make-boards (rest lines))))
