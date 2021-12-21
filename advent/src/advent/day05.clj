8ee53cb7f955a281c0c9d8f987c077d6b2c71fda
Jorge Duenas <jorge@duenas.at>
2021-12-21 01:29:59 +0100

    can parse both input files

--- advent/src/advent/day05.clj
+++ advent/src/advent/day05.clj
@@ -3,13 +3,30 @@
             [clojure.string :as string]))
 
 
-(def lines (string/split (slurp "resources/day05-test-input.txt") #"\n"))
+(def TEST-INPUT "resources/day05-test-input.txt")
+(def REAL-INPUT "resources/day05-real-input.txt")
+
+(defrecord Coords [x1 y1 x2 y2])
 
 (defn line-to-line
-  "transforms a file line into a hydrohtermal vent line
-   TODO: sort (does not work if x1 > x2 or y1 > y2)"
+  "parses the four coordinates from a file line"
   [s]
   (let [regex #"(\d+),(\d+)\s*->\s*(\d+),(\d+)"
         [_ x1 y1 x2 y2] (re-find regex s)]
     (map #(Integer. %) [x1 y1 x2 y2])))
+
+
+(defn parse-file
+  "convert the relevant lines in the file to `Coords` instances"
+  [file-name]
+  (->> file-name
+    (slurp ,,,)
+    (#(string/split % #"\n") ,,,)
+    (map line-to-line ,,,)
+    (map #(apply ->Coords %) ,,,)
+    (filter #(or (= (:x1 %) (:x2 %)) (= (:y1 %) (:y2 %))) ,,,)
+    ))
+
+
+(parse-file REAL-INPUT)
 
