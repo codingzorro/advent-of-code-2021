583d49ec12123cfab68ecaf357fff1a6fb82dfe9
Jorge Duenas <jorge@duenas.at>
2021-12-27 15:07:49 +0100

    removed suplerfluous code

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -12,35 +12,21 @@
   (map #(+ (inc counter) %)
        (take-while #(<= % days-to-go) (iterate #(+ % 7) 0)))))
 
-
 (defn fish-maker [counter days-to-go]
   (let [birth-dates (birth-dates counter days-to-go)]
     (map #(vector 5 (- days-to-go %1 3)) birth-dates)))
 
-  ;(map #(vector 5 (- %2 %1)) (map #(+ 3 %) tage) (repeat days-to-go)))
-
-
-; (map #(apply birth-dates %) %(fishmaker tage 20))
-
-(def result "6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8")
-(count (string/split result #","))
-
 (def DAYS 80)
 (def sample [3 4 3 1 2])
 (def data (map #(vector %1 %2) sample (repeat DAYS)))
-
-
 
 (println
 (count
 (loop [all-fish data result []]
   (if (empty? all-fish)
     result
-    (do
-;     (println "**" (into (rest all-fish) (apply fish-maker (first all-fish))))
-;     (println result)
-      (recur (into (rest all-fish) (apply fish-maker (first all-fish)))
-             (conj result (first all-fish))))))
+    (recur (into (rest all-fish) (apply fish-maker (first all-fish)))
+           (conj result (first all-fish)))))
 )
 )
 
