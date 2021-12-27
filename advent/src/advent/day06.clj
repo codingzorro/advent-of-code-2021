cec848d0acfbbc079d77a3a36d19453b0e7bec0a
Jorge Duenas <jorge@duenas.at>
2021-12-27 18:35:31 +0100

    a little bit of cleanup

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -8,17 +8,21 @@
 (defn birth-dates
   "find the 'date' when a new fish are born depending on the parent's counter"
   [counter days-to-go]
-  (take-while #(<= % days-to-go)
-  (map #(+ (inc counter) %)
-       (take-while #(<= % days-to-go) (iterate #(+ % 7) 0)))))
+  (let [check #(<= % days-to-go)]
+    (take-while check
+               (map #(+ (inc counter) %) (take-while check
+                                                     (range 0 days-to-go 7))))))
 
 (defn fish-maker [counter days-to-go]
   (let [birth-dates (birth-dates counter days-to-go)]
-    (map #(vector 5 (- days-to-go %1 3)) birth-dates)))
+    (pmap #(vector 5 (- days-to-go %1 3)) birth-dates)))
 
 (def DAYS 80)
-(def sample [3 4 3 1 2])
+;(def sample [3 4 3 1 2])
+(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
 (def data (map #(vector %1 %2) sample (repeat DAYS)))
+
+
 
 (println
 (count
