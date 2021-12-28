5b243393617d5657006344ed618d6ec6ba95b4b3
Jorge Duenas <jorge@duenas.at>
2021-12-28 01:36:34 +0100

    branchless 1

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -31,14 +31,13 @@
     (vec (map #(vector %1 %2) (repeat 5) values))))
 
 (defn fm [[^Integer counter ^Integer days-to-go]]
-  (let [found-value (@fm-lookup [counter days-to-go])]
-    (if found-value
-      found-value
-      (let [new-value (fish-maker [counter days-to-go])]
-        (swap! fm-lookup assoc [counter days-to-go] new-value)
-        new-value))))
+  (or
+    (@fm-lookup [counter days-to-go])
+    (let [new-value (fish-maker [counter days-to-go])]
+            (swap! fm-lookup assoc [counter days-to-go] new-value)
+            new-value)))
 
-(def DAYS 256)
+(def DAYS 200)
 (def sample [3 4 3 1 2])
 ;(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
 (def data (vec (map #(vector %1 %2) sample (repeat DAYS))))
