9fd5ef76adf736d27fe3f9b2527be13e3b436a4c
Jorge Duenas <jorge@duenas.at>
2021-12-28 08:47:20 +0100

    more inlining

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -24,18 +24,24 @@
 
 
 (defn fish-maker [[^Integer counter ^Integer days-to-go]]
-  (let [birth-dates (bd counter days-to-go)
+  (let [birth-dates (or
+                      (@bd-lookup [counter days-to-go])
+                      (let [new-value (birth-dates counter days-to-go)]
+                            (swap! bd-lookup assoc [counter days-to-go] new-value)
+                            new-value))
         new-days-to-go (- days-to-go 3)
         values (pmap #(- new-days-to-go %) birth-dates)]
     (vec (map #(vector %1 %2) (repeat 5) values))))
 
-(def DAYS 200)
+(def DAYS 256)
 (def sample [3 4 3 1 2])
 ;(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
 (def data (vec (map #(vector %1 %2) sample (repeat DAYS))))
 
 
 
+(defn -main [& args]
+(time
 (println
 (count
 (loop [all-fish data result [] counter 0]
@@ -53,4 +59,5 @@
              (inc counter)))))
 )
 )
-
+)
+)
