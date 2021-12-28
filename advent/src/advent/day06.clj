f552f18f074ef669de5bdac4828966a807c3d5cf
Jorge Duenas <jorge@duenas.at>
2021-12-28 01:29:42 +0100

    more optimizations...

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -15,7 +15,14 @@
                (map #(+ (inc counter) %) (take-while check
                                                      (range 0 days-to-go 7))))))
 
-(def bd (memoize birth-dates))
+(defn bd [^Integer counter ^Integer days-to-go]
+  (let [found-value (@bd-lookup [counter days-to-go])]
+    (if found-value
+      found-value
+      (let [new-value (birth-dates counter days-to-go)]
+        (swap! bd-lookup assoc [counter days-to-go] new-value)
+        new-value))))
+
 
 (defn fish-maker [[^Integer counter ^Integer days-to-go]]
   (let [birth-dates (bd counter days-to-go)
@@ -31,9 +38,9 @@
         (swap! fm-lookup assoc [counter days-to-go] new-value)
         new-value))))
 
-(def DAYS 80)
-;(def sample [3 4 3 1 2])
-(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
+(def DAYS 256)
+(def sample [3 4 3 1 2])
+;(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
 (def data (vec (map #(vector %1 %2) sample (repeat DAYS))))
 
 
@@ -44,7 +51,7 @@
   (if (empty? all-fish)
     result
     (let [[the-fish & the-rest] all-fish]
-      (when (zero? (rem counter 10000000)) (println counter (count @fm-lookup)))
+;     (when (zero? (rem counter 10000000)) (println counter (count @bd-lookup) (count @fm-lookup)))
       (recur (into the-rest (fm the-fish))
              (conj result the-fish)
              (inc counter)))))
