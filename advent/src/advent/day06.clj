7d160a121537edb8a5416833b683f2834f7a4fda
Jorge Duenas <jorge@duenas.at>
2021-12-29 02:06:43 +0100

    it works but it takes too much memory

--- advent/src/advent/day06.clj
+++ advent/src/advent/day06.clj
@@ -38,29 +38,37 @@
 ;(def sample [5,1,5,3,2,2,3,1,1,4,2,4,1,2,1,4,1,1,5,3,5,1,5,3,1,2,4,4,1,1,3,1,1,3,1,1,5,1,5,4,5,4,5,1,3,2,4,3,5,3,5,4,3,1,4,3,1,1,1,4,5,1,1,1,2,1,2,1,1,4,1,4,1,1,3,3,2,2,4,2,1,1,5,3,1,3,1,1,4,3,3,3,1,5,2,3,1,3,1,5,2,2,1,2,1,1,1,3,4,1,1,1,5,4,1,1,1,4,4,2,1,5,4,3,1,2,5,1,1,1,1,2,1,5,5,1,1,1,1,3,1,4,1,3,1,5,1,1,1,5,5,1,4,5,4,5,4,3,3,1,3,1,1,5,5,5,5,1,2,5,4,1,1,1,2,2,1,3,1,1,2,4,2,2,2,1,1,2,2,1,5,2,1,1,2,1,3,1,3,2,2,4,3,1,2,4,5,2,1,4,5,4,2,1,1,1,5,4,1,1,4,1,4,3,1,2,5,2,4,1,1,5,1,5,4,1,1,4,1,1,5,5,1,5,4,2,5,2,5,4,1,1,4,1,2,4,1,2,2,2,1,1,1,5,5,1,2,5,1,3,4,1,1,1,1,5,3,4,1,1,2,1,1,3,5,5,2,3,5,1,1,1,5,4,3,4,2,2,1,3])
 (def data (vec (map #(vector %1 %2) sample (repeat DAYS))))
 
+(defn to-frequencies [a-vector]
+  (frequencies (sort a-vector)))
+
+
+(defn dummy [[a b]]
+  (+ a b))
+
+
+(defn multicall [f [kv n]]
+  (repeat n (f kv)))
+
+(->> data
+  (to-frequencies ,,,)
+  (first ,,,)
+; (multicall dummy)
+  )
+
+(multicall dummy [[2 3] 4])
 
 
 (defn -main [& args]
   (time
     (println
-      (let [all-fish (atom (reduce conj clojure.lang.PersistentQueue/EMPTY data))]
-        (loop [result 0]
-          (if (empty? @all-fish)
-            result
-            (let [the-fish (peek @all-fish)
-                  new-fish (let [[^Integer counter ^Integer days-to-go] the-fish]
-                                      (or
-                                        (@fm-lookup [counter days-to-go])
-                                        (let [new-value (fish-maker [counter days-to-go])]
-                                                (swap! fm-lookup assoc [counter days-to-go] new-value)
-                                                new-value)))]
-;             (doseq [f new-fish] (swap! all-fish conj f))
-              (swap! all-fish into new-fish)
-              (swap! all-fish pop)
-              (when (zero? (rem result 100000000)) (println result (count @bd-lookup) (count @fm-lookup)))
-              (recur (inc result))))
-            ) ; loop
-          ) ; let
+      (loop [all-fish data result 0]
+        (if (empty? all-fish)
+          result
+          (recur (reduce into [] (map fish-maker all-fish)) (+ result (count all-fish)))
+        ) ; loop
       ) ; println
     ) ; time
   ) ; main
+)
+
+
